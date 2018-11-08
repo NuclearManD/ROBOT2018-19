d8b4574282ea33a4e3176a136aab68c49378bc90
Alpakah <hahnsuk@gmail.com>
2018-11-08 07:46:21 -0800

    it's all guci

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/ArmDriver.java
@@ -0,0 +1,15 @@
+
+package org.firstinspires.ftc.teamcode.drivers;
+
+
+public class ArmDriver {
+
+    public static void main() {
+
+
+        void extend(float distance);{
+        }
+        void openClaw()
+
+    }
+}
