9cc6ba609e016c2b46c7dabeb66717c30cea6580
shanempage <shanempage@gmail.com>
2018-11-08 10:20:36 -0800

    Update TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/CameraInput.java
    Beginning of CameraInput.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/CameraInput.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.drivers;
+
+import android.graphics.Bitmap;
+import android.hardware.camera2.CameraDevice;
+
+/**
+ * Created by SCRoboticsDev on 11/8/2018.
+ */
+
+public class CameraInput {
+    CameraDevice cam;
+    public CameraInput() {
+        cam.createCaptureSession();
+    }
+
+    public Bitmap takePic(){
+
+    }
+}
