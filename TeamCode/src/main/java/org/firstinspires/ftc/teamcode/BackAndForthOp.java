d6b55f4970ea9d052f6310d187336b8b2e913e6f
NuclearManD <dylanbrophy@gmail.com>
2018-11-09 07:08:11 -0800

    Rescued the mecanum driver, making new opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BackAndForthOp.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.drivers.MovementDriver;
+
+public class BackAndForthOp extends LinearOpMode {
+    static final float FORWARD_SPEED = -0.6f;
+    public void runOpMode(){
+        try {
+
+        }catch (Exception e){
+            System.out.println("\n------    HARDWARE ERROR IN INIT!   ------\n");
+            e.printStackTrace();
+        }
+        try {
+            waitForStart();
+        }catch (Exception e) {
+            e.printStackTrace();
+
+        }
+    }
+}
