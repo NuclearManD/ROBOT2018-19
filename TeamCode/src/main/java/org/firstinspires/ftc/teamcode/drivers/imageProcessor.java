3014b75ccfcc21ce26ab7a2bf6f541d54b41f9e9
sam <44331094+sinisterhandsoap@users.noreply.github.com>
2018-11-08 17:12:03 -0800

    just more skelly bois

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/imageProcessor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/imageProcessor.java
@@ -3,7 +3,11 @@ package org.firstinspires.ftc.teamcode.drivers;
 
 public class imageProcessor {
 
-    
+    public static void main(CameraInput){
+        int getLandingSpot[];
+
+        int getMineralLocation[];
+    }
 
 
 }
