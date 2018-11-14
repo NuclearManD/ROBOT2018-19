665a8ad5348e058304338b262ae2181bb855e952
Armweakest <42901687+Armweakest@users.noreply.github.com>
2018-11-14 07:34:02 -0800

    Update TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/UltimateDriver.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/UltimateDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/UltimateDriver.java
@@ -1,45 +1,50 @@
 package org.firstinspires.ftc.teamcode.drivers;
-import java.util.Math;
+
+import java.lang.Math;
 
 public class UltimateDriver extends Mecanum4WheelDriver {
     private double vfl;//vfl = "velocity front left" wheel before adding rotational movement; vbl, vfr, and vbr follow the same scheme
     private double vbl;
-    private double vfl;
-    private double vbl;
+    private double vfr;
+    private double vbr;
     private double vflFinal;//velocity of a wheel AFTER rotational movement is added
     private double vblFinal;
     private double vfrFinal;
     private double vbrFinal;
     private double robotAngle;
-    public void autoDrive(double angleInRaidans, double speed) {//move the robot in any straight direction in any given angle (in radians) and speed (0 to 1)
+    public void autoDrive(double angleInRadians, double speed) {//move the robot in any straight direction in any given angle (in radians) and speed (0 to 1)
         //0 radians is moving the robot straight ahead (+y direction) angleInRadians changes counter-clockwise from 0; @param angleInRadians must be [0, 6.28) radians
-        if (angleInRaidans >= 0 || angleInRaidans < 2*Math.PI) {//if @param angleInRadians falls within 0 to 2pi
-            robotAngle = angleInRaidans+(Math.PI/4);
-            vfl = 3(speed * Math.cos(robotAngle))/4;
-            vbl = 3(speed * Math.sin(robotAngle))/4;
-            vfr = 3(speed * Math.sin(robotAngle))/4;
-            vbr = 3(speed * Math.cos(robotAngle))/4;
+        if (angleInRadians >= 0 || angleInRadians < 2*Math.PI) {//if @param angleInRadians falls within 0 to 2pi
+            robotAngle = angleInRadians+(Math.PI/4);
+            vfl = .75*(speed * Math.cos(robotAngle));
+            vbl = .75*(speed * Math.sin(robotAngle));
+            vfr = .75*(speed * Math.sin(robotAngle));
+            vbr = .75*(speed * Math.cos(robotAngle));
             motorSet(vfl, vbl, vfr, vbr);
         }
     }
     public void manualDrive(double gamepadX, double gamepadY){//put in gamepad inputs for gamepadX and gamepadY
-        double hypotenuse = Math.hypot(gamepadX, gamepad1Y);//finds the overall velocity desired based on the controller inputs
+        double hypotenuse = Math.hypot(gamepadX, gamepadY);//finds the overall velocity desired based on the controller inputs
         robotAngle = Math.atan2(gamepadY, gamepadX) - Math.PI / 4;//turns the angle pi/4 clockwise to adjust for the orientation of the mecanum wheels
-        vfl = 3(hypotenuse * Math.cos(robotAngle))/4;
-        vbl = 3(hypotenuse * Math.sin(robotAngle))/4;
-        vfr = 3(hypotenuse * Math.sin(robotAngle))/4;
-        vbr = 3(hypotenuse * Math.cos(robotAngle))/4;
+        vfl = .75*(hypotenuse * Math.cos(robotAngle));
+        vbl = .75*(hypotenuse * Math.sin(robotAngle));
+        vfr = .75*(hypotenuse * Math.sin(robotAngle));
+        vbr = .75*(hypotenuse * Math.cos(robotAngle));
         motorSet(vfl, vbl, vfr, vbr);
     }
-    public static void autoTurn(boolean turnLeft, boolean turnRight){//turn the robot in place; combined with motion it will turn?
+    public void autoTurn(boolean turnLeft, boolean turnRight){//turn the robot in place; combined with motion it will turn?
         if (turnLeft&&!turnRight){
-            vflFinal, vblFinal = vf1-.25;//.25 is the amount out of 1 that the wheels dedicate to rotation instead of directional movement
-            vfrFinal, vbrFinal = vfr+.25;
+            vflFinal = vfl-.25;//.25 is the amount out of 1 that the wheels dedicate to rotation instead of directional movement
+            vblFinal = vbl-.25;
+            vfrFinal = vfr+.25;
+            vbrFinal = vbr+.25;
             motorSet(vflFinal, vblFinal, vfrFinal, vbrFinal);
         }
         if (turnRight&&!turnLeft){
-            vflFinal, vblFinal = vf1+.25;
-            vfrFinal, vbrFinal = vfr-.25;
+            vflFinal = vfl-.25;
+            vblFinal = vbl-.25;
+            vfrFinal = vfr+.25;
+            vbrFinal = vbr+.25;
             motorSet(vflFinal, vblFinal, vfrFinal, vbrFinal);
         }
     }
