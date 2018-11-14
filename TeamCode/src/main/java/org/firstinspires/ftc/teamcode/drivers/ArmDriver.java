164cfa0eefd02cce0e7ffeaead7c99cde23dfa44
Alpakah <hahnsuk@gmail.com>
2018-11-14 09:49:51 -0800

    try sleeping fole

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/ArmDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/ArmDriver.java
@@ -1,10 +1,27 @@
 
 package org.firstinspires.ftc.teamcode.drivers;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class ArmDriver {
 
+        DcMotor pully;
+        DcMotor ang;
         void extend(float distance){
+             try {
+                     pully.setPower(1);
+
+                     Thread.sleep(200);
+
+             } catch (Exception e){
+                     
+             }
+
+
+
+
+
+
 
         }
         void openClaw() {
