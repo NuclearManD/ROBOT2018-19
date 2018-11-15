6673175ddf9820e5be4953c49cb2fbf252a3aa0a
Alpakah <hahnsuk@gmail.com>
2018-11-14 19:26:34 -0800

    hey i did a thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/ArmDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/ArmDriver.java
@@ -2,6 +2,7 @@
 package org.firstinspires.ftc.teamcode.drivers;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class ArmDriver {
 
@@ -10,26 +11,24 @@ public class ArmDriver {
         void extend(float distance){
              try {
                      pully.setPower(1);
-
-                     Thread.sleep(200);
-
+                     Thread.sleep((long)(distance*DIS_COVERSION));
              } catch (Exception e){
-                     
+                     System.out.println("oog something got gooned");
              }
 
-
-
-
-
-
-
+             pully.setPower(0);
         }
-        void openClaw() {
+        public static final float DIS_COVERSION= 1;
+        // this constant is tmp plz change and test
+
+       Servo rot;
+       Servo colect;
+        void openScoop() {
+
+                rot. = 0;;
         }
 
-        void closeClaw(){
 
-        }
         void  rotate(float degrees){
 
         }
