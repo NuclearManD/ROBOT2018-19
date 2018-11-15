db09d5ce1954acf272e66edf6a58dc0aa96b6c35
Armweakest <42901687+Armweakest@users.noreply.github.com>
2018-11-14 19:21:34 -0800

    Gamepad
    ControllerOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadTest.java
@@ -2,14 +2,45 @@ package org.firstinspires.ftc.teamcode;
 
 /**
  * Created by SCRoboticsDev on 11/14/2018.
+ * GamePad Documentation: ftckey...com
  */
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.drivers.Mecanum4WheelDriver;
 import org.firstinspires.ftc.teamcode.drivers.UltimateDriver;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
-public class GamepadTest {
+@TeleOp(name="GamepadTest", group="2018")
+public class GamepadTest extends LinearOpMode {
+    static final float FORWARD_SPEED = -0.6f;
+    public void runOpMode(){
+        UltimateDriver driver = new UltimateDriver();
+        Gamepad movePad = gamepad1;
+        Gamepad liftPad = gamepad2;
+        try {
+            DcMotor[] motors = {hardwareMap.dcMotor.get("fl"),hardwareMap.dcMotor.get("fr"),hardwareMap.dcMotor.get("bl"),hardwareMap.dcMotor.get("br")};
+            driver.init(motors);
+        }catch (Exception e){
+            System.out.println("\n------    HARDWARE ERROR IN INIT!   ------\n");
+            e.printStackTrace();
+        }
+        try {
+            waitForStart();
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        while (true) {
+            try {
+                driver.manualDrive(movePad.left_stick_x, movePad.left_stick_y);
+            }
+            catch (Exception e){
+                telemetry.addLine("ERROR IN MAIN CODE!!!!");
+                return;
+            }
+        }
+    }
 }
