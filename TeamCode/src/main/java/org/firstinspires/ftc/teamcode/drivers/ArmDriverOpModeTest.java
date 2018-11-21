5ce2ba400755825d1371d89bb1776a55a9b7ab13
Alpakah <hahnsuk@gmail.com>
2018-11-21 10:18:34 -0800

    lets try to do a thing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/ArmDriverOpModeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/ArmDriverOpModeTest.java
@@ -15,8 +15,8 @@ public class ArmDriverOpModeTest extends LinearOpMode {
 
         try {
             pully = hardwareMap.dcMotor.get("fl");
-            ArmDriver driver = new ArmDriver(pully, hardwareMap.dcMotor.get("fl"));
-            pully.setPower(1);
+            ArmDriver driver = new ArmDriver(pully, hardwareMap.dcMotor.get("fr"));
+           driver.rotate(215);
         } catch (Exception e) {
             System.out.println("oog something got gooned");
         }
