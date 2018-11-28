96ac80faf058683bddd6c37783e72da447949ac4
shanempage <shanempage@gmail.com>
2018-11-27 17:31:39 -0800

    AHHHHH FIXES NOOOOO

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/Multitasker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/Multitasker.java
@@ -2,13 +2,37 @@ package org.firstinspires.ftc.teamcode.drivers;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import java.util.ArrayList;
+
 public class Multitasker {
-    LinearOpMode master;
+    private LinearOpMode master;
+
+    private ArrayList<Long> taskTimes = new ArrayList<Long>();
+    private ArrayList<Task> tasks = new ArrayList<Task>();
+
+    int currentTask = -1;
+
+    public void taskSleep(long time) {
+        taskTimes.set(currentTask,System.currentTimeMillis()+time);
+    }
+
+    public void addTask(Task t){
+        tasks.add(t);
+        taskTimes.add(new Long(0));
+    }
+
     public Multitasker(LinearOpMode you){
         master = you;
     }
-    public void yield(){
 
+    public void yield(){
+        for(currentTask=0;currentTask<tasks.size();currentTask++){
+            Task t = tasks.get(currentTask);
+            if(taskTimes.get(currentTask)<System.currentTimeMillis()){
+                t.update(this);
+            }
+        }
+        currentTask = -1;
     }
     public void waitTime(long time){
         time+=System.currentTimeMillis();
@@ -18,10 +42,8 @@ public class Multitasker {
                 return;
             }
             try{
-                Thread.sleep(5);
-            }catch(Exception e){
-
-            }
+                Thread.sleep(2);
+            }catch(Exception e){}
         }
     }
 }
