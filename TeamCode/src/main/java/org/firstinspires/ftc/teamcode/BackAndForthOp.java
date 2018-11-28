7ad80e2492d9273322f5c8a8606b33fe4cc68a7f
NuclearManD <dylanbrophy@gmail.com>
2018-11-27 19:39:51 -0800

    opmode prototype (skeleboi)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BackAndForthOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BackAndForthOp.java
@@ -10,7 +10,7 @@ import org.firstinspires.ftc.teamcode.drivers.MovementDriver;
 import org.firstinspires.ftc.teamcode.drivers.Multitasker;
 
 
-@TeleOp(name="Back'n Forth", group="2018")
+//@TeleOp(name="Back'n Forth", group="2018")
 public class BackAndForthOp extends LinearOpMode {
     static final float FORWARD_SPEED = -0.6f;
     public void runOpMode(){
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualOp.java
@@ -1,11 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.teamcode.drivers.Mecanum4WheelDriver;
+import org.firstinspires.ftc.teamcode.drivers.Multitasker;
+
+
+@TeleOp(name="Main Op Mode", group="2018")
 public class ManualOp extends LinearOpMode {
 
     @Override
     public void runOpMode() {
+        Mecanum4WheelDriver driver = new Mecanum4WheelDriver();
+        Multitasker multi = new Multitasker(this);
+        DcMotor[] motors = {hardwareMap.dcMotor.get("fl"),hardwareMap.dcMotor.get("fr"),hardwareMap.dcMotor.get("bl"),hardwareMap.dcMotor.get("br")};
+        driver.init(motors,-1,.1f);
+        multi.addTask(driver);
 
+        waitForStart();
+
+        while(opModeIsActive()){
+            float y = gamepad1.right_stick_y;
+            float x = gamepad1.right_stick_x;
+            float R = gamepad1.left_stick_x;
+            driver.setY(y);
+            driver.setX(x);
+            driver.setR(R);
+        }
     }
 }
