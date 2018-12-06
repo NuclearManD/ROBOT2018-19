d690eba13e4e563fa82194a05750e26314b54702
Alpakah <hahnsuk@gmail.com>
2018-12-05 18:41:02 -0800

    if your going then go (go go go)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOp.java
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 
 import org.firstinspires.ftc.teamcode.drivers.ArmDriver;
 import org.firstinspires.ftc.teamcode.drivers.Mecanum4WheelDriver;
@@ -19,6 +21,8 @@ public class AutoOp extends LinearOpMode{
     public void runOpMode() throws InterruptedException {
 
         // initailize here
+        ColorSensor sensorColor;
+        DistanceSensor sensorDistance;
 
         Mecanum4WheelDriver driver = new Mecanum4WheelDriver();
         ArmDriver arm = new ArmDriver(hardwareMap.dcMotor.get("pully"),hardwareMap.dcMotor.get("angle"),hardwareMap.crservo.get("goboi"));
@@ -28,11 +32,19 @@ public class AutoOp extends LinearOpMode{
         multi.addTask(driver);
         multi.addTask(arm);
         multi.addTask(new TelemetryUpdater());
+        sensorColor = hardwareMap.get(ColorSensor.class, "sensor_color_distance");
+
 
         waitForStart();
 
         // run the autonomous pgm
 
-        
+        driver.setY(.5);
+        multi.waitTime(500);
+        driver.setY(0);
+
+        //sensorColor.green();
+
+
     }
 }
