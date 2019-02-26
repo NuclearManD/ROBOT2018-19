7e4047403bf2a6b47e884804d9632fc343203263
NuclearManD <dylanbrophy@gmail.com>
2019-02-25 17:43:28 -0800

    lol g00n

--- TeamCode/src/main/java/TestsAndLegacy/TestMRDistanceSensor.java
+++ TeamCode/src/main/java/TestsAndLegacy/TestMRDistanceSensor.java
@@ -1,18 +1,26 @@
 package TestsAndLegacy;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.drivers.MRDistanceSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
-@TeleOp(name="Test Distaancce Senseerz")
+@TeleOp(name = "test MR range sensor", group = "Sensor")
 public class TestMRDistanceSensor extends LinearOpMode {
-    @Override
-    public void runOpMode() {
-        MRDistanceSensor sensor = new MRDistanceSensor();
-        sensor.setup(hardwareMap.i2cDevice.get("distance"));
-        while(true){
-            telemetry.addLine("->"+sensor.getDistance());
+
+    ModernRoboticsI2cRangeSensor rangeSensor;
+
+    @Override public void runOpMode() {
+
+        rangeSensor = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "distance");
+
+        // wait for the start button to be pressed
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("x", "%.2f cm", rangeSensor.getDistance(DistanceUnit.CM));
             telemetry.update();
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/MRDistanceSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drivers/MRDistanceSensor.java
@@ -1,29 +1,16 @@
 package org.firstinspires.ftc.teamcode.drivers;
 
-import com.qualcomm.robotcore.hardware.I2cAddr;
-import com.qualcomm.robotcore.hardware.I2cDevice;
-import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
-import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class MRDistanceSensor {
-    byte[] range1Cache; //The read will return an array of bytes. They are stored in this variable
 
-    I2cAddr RANGE1ADDRESS = new I2cAddr(0x14); //Default I2C address for MR Range (7-bit)
-    public static final int RANGE1_REG_START = 0x04; //Register to start reading
-    public static final int RANGE1_READ_LENGTH = 2; //Number of byte to read
-
-    public I2cDevice RANGE1;
-    public I2cDeviceSynch RANGE1Reader;
-    public void setup(I2cDevice sensor){
-        RANGE1 = sensor;
-        RANGE1Reader = new I2cDeviceSynchImpl(RANGE1, RANGE1ADDRESS, false);
-        RANGE1Reader.engage();
+    public ModernRoboticsI2cRangeSensor sensor;
+    public void setup(ModernRoboticsI2cRangeSensor sensor){
+        this.sensor=sensor;
     }
-    public int getDistance(){
-        range1Cache = RANGE1Reader.read(RANGE1_REG_START, RANGE1_READ_LENGTH);
-
-        int a = range1Cache[0] & 0xFF; // ultrasonic
-        int b = range1Cache[1] & 0xFF; // ir
-        return a;
+    public double getDistance(){
+        return sensor.getDistance(DistanceUnit.CM);
     }
 }
